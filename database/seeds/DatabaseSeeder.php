eedd2b29d0b35f330e4399ff0801ae4e9d3e9648
amar <kharwadeamar@gmail.com>
2019-11-07 12:13:10 +0530

    updated seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $role=['superadmin','admin','inventory manager','order manager','customer'];
+        for ($i=0; $i < 5; $i++) { 
+            DB::table('roles')->insert([
+                'role_name' => $role[$i],
+                'created_at'=>date("Y-m-d H:i:s"),
+                'updated_at'=>date("Y-m-d H:i:s"),
+            ]);
+        }
         
         DB::table('users')->insert([
             
@@ -18,8 +26,9 @@ class DatabaseSeeder extends Seeder
             'lastname' =>'admin',
             'email' =>'kharwadeamar@gmail.com',
             'password' =>bcrypt('admin123'),
-            'roles'=>2,
+            'roles'=>1,
             'status'=>'0',
+            'image'=>'',
             'created_at'=>date("Y-m-d H:i:s"),
             'updated_at'=>date("Y-m-d H:i:s"),
         ]); 
